0dc09f3955cf8debb873542e0d410f4cfcaca8a4
Joaquin <jgarcias.cr@gmail.com>
2026-02-25 13:47:59 +0700

    feat: enhance autoloading mechanism and add composer.lock for dependency management

--- public/index.php
+++ public/index.php
@@ -6,7 +6,29 @@ $autoload = __DIR__ . '/../vendor/autoload.php';
 if (is_file($autoload)) {
    require $autoload;
 } else {
-   require __DIR__ . '/../../framework/src/bootstrap.php';
+   $rootAutoload = __DIR__ . '/../../../vendor/autoload.php';
+   if (is_file($rootAutoload)) {
+      require $rootAutoload;
+   } else {
+      require __DIR__ . '/../../framework/src/bootstrap.php';
+   }
+
+   spl_autoload_register(static function (string $class): void {
+      $prefix = 'App\\';
+      if (!str_starts_with($class, $prefix)) {
+         return;
+      }
+
+      $relative = substr($class, strlen($prefix));
+      if ($relative === false) {
+         return;
+      }
+
+      $path = __DIR__ . '/../app/' . str_replace('\\', '/', $relative) . '.php';
+      if (is_file($path)) {
+         require $path;
+      }
+   });
 }
 
 use App\AppServiceProvider;
